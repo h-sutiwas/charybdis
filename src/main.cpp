c8482dbb0b08543cb5e9654f6cdddb74a26640f1
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2026-03-25 18:46:35 +0700

    Update main.cpp:
    - verify linkages of glad and GLFW and check the version of OpenGL.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,52 @@
 //
-// Created by UsEr on 2026-03-23.
+// Created by h-sutiwas on 2026-03-23.
 //
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../external/stb/stb_image.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <windows.h>
+
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+const int WIDTH = 1920, HEIGHT = 1080;
+
+int main(){
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Charybdis", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    glfwTerminate();
+
+    return 0;
+}
